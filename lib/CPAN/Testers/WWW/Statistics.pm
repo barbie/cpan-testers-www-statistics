72bc11d40efe75417a7c1404ed0e35f1f1143b92
Barbie <barbie@missbarbell.co.uk>
2011-08-18 13:15:17 +0100

    updated banner and missing layout images added

--- lib/CPAN/Testers/WWW/Statistics.pm
+++ lib/CPAN/Testers/WWW/Statistics.pm
@@ -5,7 +5,7 @@ use warnings;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '0.93';
+$VERSION = '0.94';
 
 #----------------------------------------------------------------------------
 
--- lib/CPAN/Testers/WWW/Statistics/Graphs.pm
+++ lib/CPAN/Testers/WWW/Statistics/Graphs.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = '0.93';
+$VERSION = '0.94';
 
 #----------------------------------------------------------------------------
 
