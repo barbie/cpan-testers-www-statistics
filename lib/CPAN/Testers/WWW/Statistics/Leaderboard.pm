66477bfc0ff75eb4a4c9ca754a15aaa3f26804e7
Barbie <barbie@missbarbell.co.uk>
2015-02-07 17:41:40 +0000

    updated copyright year

--- lib/CPAN/Testers/WWW/Statistics/Leaderboard.pm
+++ lib/CPAN/Testers/WWW/Statistics/Leaderboard.pm
@@ -297,7 +297,7 @@ F<http://wiki.cpantesters.org/>
 
 =head1 COPYRIGHT AND LICENSE
 
-  Copyright (C) 2005-2014 Barbie for Miss Barbell Productions.
+  Copyright (C) 2005-2015 Barbie for Miss Barbell Productions.
 
   This distribution is free software; you can redistribute it and/or
   modify it under the Artistic Licence v2.
